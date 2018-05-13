8c12dcf4edb2eb9b4ec31bc084eac09489336490
Wooseung <aswe11@korea.ac.kr>
2018-05-13 18:48:27 +0900

    git push

---
+++ run.sh
@@ -0,0 +1,52 @@
+
+#nice ./bin/Linux-g++/main run.mac conf/e72/p724_t1.conf rootfile/p724_t1.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/p725_t1.conf rootfile/p725_t1.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/p730_t1.conf rootfile/p730_t1.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/p735_t1.conf rootfile/p735_t1.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/p740_t1.conf rootfile/p740_t1.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/p745_t1.conf rootfile/p745_t1.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/p750_t1.conf rootfile/p750_t1.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/p755_t1.conf rootfile/p755_t1.root
+
+#nice ./bin/Linux-g++/main run.mac conf/e72/mf/p730_t1.0.conf rootfile/mf_test/p730_t1.0.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/mf/p730_t0.9.conf rootfile/mf_test/p730_t0.9.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/mf/p730_t0.8.conf rootfile/mf_test/p730_t0.8.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/mf/p730_t0.7.conf rootfile/mf_test/p730_t0.7.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/mf/p730_t0.6.conf rootfile/mf_test/p730_t0.6.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/mf/p730_t0.5.conf rootfile/mf_test/p730_t0.5.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/mf/p730_t0.0.conf rootfile/mf_test/p730_t0.0.root
+
+#nice ./bin/Linux-g++/main run.mac conf/e72/trigger/p730_t0.6.conf rootfile/trigger/p730_t0.6.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/trigger/p730_t0.6_electron.conf rootfile/trigger/p730_t0.6_electron.root
+
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p730_zm1000_t0.0.conf rootfile/vp/p730_zm1000_t0.0.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p730_zm1000_f0.5.conf rootfile/vp_test/p730_zm1000_t0.5.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p730_zm1000_f0.6.conf rootfile/vp_test/p730_zm1000_t0.6.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p730_zm1000_f0.7.conf rootfile/vp_test/p730_zm1000_t0.7.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p730_zm1000_f0.8.conf rootfile/vp_test/p730_zm1000_t0.8.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p730_zm1000_f0.9.conf rootfile/vp_test/p730_zm1000_t0.9.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p730_zm1000_f1.0.conf rootfile/vp_test/p730_zm1000_t1.0.root
+
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p725_zm1000_f0.6.conf rootfile/vp_test/p725_zm1000_t0.6.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p728_zm1000_f0.6.conf rootfile/vp_test/p728_zm1000_t0.6.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p732_zm1000_f0.6.conf rootfile/vp_test/p732_zm1000_t0.6.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p735_zm1000_f0.6.conf rootfile/vp_test/p735_zm1000_t0.6.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p740_zm1000_f0.6.conf rootfile/vp_test/p740_zm1000_t0.6.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p750_zm1000_f0.6.conf rootfile/vp_test/p750_zm1000_t0.6.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/vp/p760_zm1000_f0.6.conf rootfile/vp_test/p760_zm1000_t0.6.root
+
+#Kaon decay
+#nice ./bin/Linux-g++/main run.mac conf/e72/trigger/p730_zm650_f0.6.conf rootfile/trigger/p730_zm650_t0.6_nohadron.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/trigger/p730_zm650_f1.0.conf rootfile/trigger/p730_zm650_t1.0_nohadron.root
+
+#accept
+#nice ./bin/Linux-g++/main run.mac conf/e72/accept/p725_t0.6_electron.conf rootfile/accept/p725_t0.6_nohadron.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/accept/p728_t0.6_electron.conf rootfile/accept/p728_t0.6_nohadron.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/accept/p730_t0.6_electron.conf rootfile/accept/p730_t0.6_nohadron.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/accept/p732_t0.6_electron.conf rootfile/accept/p732_t0.6_nohadron.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/accept/p735_t0.6_electron.conf rootfile/accept/p735_t0.6_nohadron.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/accept/p740_t0.6_electron.conf rootfile/accept/p740_t0.6_nohadron.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/accept/p750_t0.6_electron.conf rootfile/accept/p750_t0.6_nohadron.root
+#nice ./bin/Linux-g++/main run.mac conf/e72/accept/p760_t0.6_electron.conf rootfile/accept/p760_t0.6_nohadron.root
+
+nice ./bin/Linux-g++/main run.mac conf/e72/p730_t1.conf rootfile/p730_t1.root
